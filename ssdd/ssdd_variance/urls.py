2a599977308797f9c82225df028f94e724ddc350
Darshini0402 <darshini.r0402@gmail.com>
2021-05-27 19:15:00 +0530

    views_urls

--- ssdd/ssdd_variance/urls.py
+++ ssdd/ssdd_variance/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.firstpage,name='firstpage'),
+    path('Login.html', views.Login,name='Login'),
+    path('firstpage.html', views.firstpage,name='firstpage'),
+    path('Admin.html', views.Admin,name='Admin'),
+    path('User.html', views.User,name='User'),
     path('sign.html', views.sign,name="sign"),
     path('faq.html', views.faq,name='faq'),
     path('bagsnmeals.html', views.bagsnmeals,name='bagsnmeals'),
