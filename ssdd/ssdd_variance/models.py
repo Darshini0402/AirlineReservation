a48158f854d688fcab7a3ec6568ce91249b725c9
Darshini0402 <darshini.r0402@gmail.com>
2021-06-06 22:22:29 +0530

    flights_airports

--- ssdd/ssdd_variance/models.py
+++ ssdd/ssdd_variance/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class airports(models.Model):
+    code=models.CharField(max_length=3, primary_key=True)
+    airport_name=models.CharField(max_length=64)
+    city=models.CharField(max_length=10)
+    
+    def __str__(self):
+        return f"{self.code} {self.airport_name} {self.city}"
+
+class flights(models.Model):
+    flightno=models.CharField(max_length=7,primary_key=True)
+    origin=models.ForeignKey(airports,on_delete=models.CASCADE, related_name='departure')
+    destination=models.ForeignKey(airports,on_delete=models.CASCADE, related_name='arrival')
+    duration=models.IntegerField()
+    business=models.IntegerField()
+    economy=models.IntegerField()
