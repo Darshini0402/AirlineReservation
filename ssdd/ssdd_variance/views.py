9eb412a14ecb40a4e7f0d539a18819ac6e233e95
Darshini0402 <darshini.r0402@gmail.com>
2021-06-09 13:28:21 +0530

    REVIEW 1

--- ssdd/ssdd_variance/views.py
+++ ssdd/ssdd_variance/views.py
@@ -1,30 +1,57 @@
-from .models import airports, flights, user_login
-from django.http.response import HttpResponse
+from .models import airports, flights, user_login, Departs
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def firstpage(request):
-    answer = request.POST.get('fromcity')
-    
+    departure=request.GET["fromcities"]
+    arrival=request.GET["tocities"]
+    date=request.GET["deptdate"]
+    passenger=request.GET["count"]
     return render(request,'firstpage.html',{
-        "cities": airports.objects.all(), "choice": answer
+        "cities": airports.objects.all(), "deptplace":departure, "arrplace":arrival
     })
 
-"""class fromcity(CreateView):
-    model = airports
-    form_class = PersonForm
-    success_url = reverse_lazy('firstpage')
+def search(request):
+    return render(request,'searchflight.html',{
+        "departs": Departs.objects.all()
+    })
 
-class tocity(UpdateView):
-    model = airports
-    form_class = PersonForm
-    success_url = reverse_lazy('firstpage')
-def load_cities(request):
-    country_id = request.GET.get('from')
-    city = airports.objects.filter(country_id=country_id).order_by('city')
-    return render(request, 'firstpage.html', {'cities': city})"""
+def list(request):
+    return render(request,'firstpage.html',{
+        "cities": airports.objects.all()
+    })
+
+def index(request):
+    # If no user is signed in, return to login page:
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request,"firstpage.html")
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+            # Otherwise, return login page again with new context
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid Credentials"
+            })
+    return render(request, "login.html")
+def logout_view(request):
+    logout(request)
+    return render(request, "login.html", {
+                "message": "Logged Out"
+            })
+    
 
 
 def sign(request):
@@ -51,15 +78,10 @@ def faq(request):
     return render(request,'faq.html')
 def aboutus(request):
     return render(request,'aboutus.html')
-def Admin(request):
-    return render(request,'Admin.html')
+
 def Login(request):
     return render(request,'Login.html')
 def User(request):
     return render(request,'User.html')
-def search(request):
-    return render(request,'searchflight.html',{
-        "flights" : flights.objects.all()
-    })
 
 
